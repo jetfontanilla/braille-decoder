3ece350e849fb0ba5fc892d566d0d5aea5968fc8
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2015-07-24 11:37:28 -0400

    Final Solution

--- js/decoder.js
+++ js/decoder.js
@@ -1,3 +1,10 @@
+function rawToBrailleBinary(raw) {
+    return raw.map(function(rawLine) {
+        return rawLine.replace(/O/g, '1').replace(/\./g, '0');
+    })
+}
+
+var BRAILLE_CHUNK_SIZE = 3;
 var BRAILLE_LOOKUP = {
     100000: 'a',
     101000: 'b',
@@ -27,18 +34,39 @@ var BRAILLE_LOOKUP = {
     100111: 'z'
 };
 
-var secretMessage = [
-    '.O O. .O OO O. O. .O OO O. OO O. .O O. .O',
-    'O. .O O. .. .. OO OO .. .. .. OO OO .O OO',
-    'O. O. O. O. .. O. O. O. OO .. .. .O O. .O'
-];
+function parseBrailleBinary(binary) {
+    var decoded;
+    var currentHash = binary.reduce(function(acc, line) {
+        return acc + line.substr(0, BRAILLE_CHUNK_SIZE).trim();
+    }, '');
 
-function rawToBinary(raw) {
-    return _.map(raw, function(rawLine) {
-        return rawLine.replace('O', '1').replace('.', '0');
-    })
+    if (!currentHash.length) {
+        return '';
+    }
+
+    if (currentHash.length && currentHash in BRAILLE_LOOKUP) {
+        decoded = BRAILLE_LOOKUP[currentHash];
+    } else {
+        decoded = '';
+    }
+
+    var tail = binary.map(function(line) {
+        var lineLength = line.length;
+        if (lineLength < BRAILLE_CHUNK_SIZE) {
+            return '';
+        }
+
+        return line.substring(BRAILLE_CHUNK_SIZE);
+    });
+
+    return decoded + parseBrailleBinary(tail);
 }
 
 
+var secretMessage = [
+    '.O O. .O OO O. O. .O OO O. OO O. .O O. .O ',
+    'O. .O O. .. .. OO OO .. .. .. OO OO .O OO ',
+    'O. O. O. O. .. O. O. O. OO .. .. .O O. .O '
+];
 
-rawToBinary(secretMessage);
+alert(parseBrailleBinary(rawToBrailleBinary(secretMessage)));
